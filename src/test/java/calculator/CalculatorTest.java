3092a60468bba48a08723ea171bae36b0ce4a81d
Bas Dijkstra <bas@ontestautomation.com>
2022-06-11 08:52:04 +0200

    Add data driven test example

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,10 +1,31 @@
 package calculator;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    
+    @DataProvider(name = "calculatorTestData")
+    public Object[][] createData() {
+
+        return new Object[][] {
+                {2, 2, 4},
+                {1, 1, 2},
+                {5, 4, 9}
+        };
+    }
+
+    @Test(dataProvider = "calculatorTestData")
+    public void add(int one, int two, int total) {
+
+        Calculator calc = new Calculator();
+
+        calc.add(one);
+
+        calc.add(two);
+
+        assertEquals(total, calc.getTotal());
+    }
 }
