dbe2bfc03301894be5f5c89751c4f7e71f9fbd62
Bas Dijkstra <bas@ontestautomation.com>
2022-06-11 08:33:54 +0200

    Remove examples

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -6,15 +6,5 @@ import static org.testng.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    @Test
-    public void add2and2_checkTotal_shouldBeEqualTo4() {
-
-        Calculator calculator = new Calculator();
-
-        calculator.add(2);
-
-        calculator.add(2);
-
-        assertEquals(4, calculator.getTotal());
-    }
+    
 }
